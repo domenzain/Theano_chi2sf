Theano/Theano_lgpl

First version of chi2sf Op with C code

theano_lgpl/__init__.py
@@ -1,0 +1,1 @@
+import theano_lgpl.elemwise_c

theano_lgpl/tests/test_elemwise_c.py
@@ -1,0 +1,14 @@
+from theano.tensor.tests.test_basic import (
+    expected_chi2sf, _good_broadcast_unary_chi2sf,
+    makeBroadcastTester, mode_no_scipy, skip_scipy)
+
+from theano_lgpl.elemwise_c import cchi2sf
+
+CChi2SFTester = makeBroadcastTester(
+    op=cchi2sf,
+    expected=expected_chi2sf,
+    good=_good_broadcast_unary_chi2sf,
+    eps=2e-10,
+    mode=mode_no_scipy,
+    skip=skip_scipy,
+    name='CChi2SFTester')

domenzain/Theano_chi2sf

theano_chi2sf/__init__.py
@@ -1,0 +1,1 @@
+import theano_chi2sf.elemwise_c

theano_chi2sf/tests/test_elemwise_c.py
@@ -1,0 +1,14 @@
+from theano.tensor.tests.test_basic import (
+    expected_chi2sf, _good_broadcast_unary_chi2sf,
+    makeBroadcastTester, mode_no_scipy, skip_scipy)
+
+from theano_chi2sf.elemwise_c import cchi2sf
+
+CChi2SFTester = makeBroadcastTester(
+    op=cchi2sf,
+    expected=expected_chi2sf,
+    good=_good_broadcast_unary_chi2sf,
+    eps=2e-10,
+    mode=mode_no_scipy,
+    skip=skip_scipy,
+    name='CChi2SFTester')